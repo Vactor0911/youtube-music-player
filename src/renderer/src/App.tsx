25a8cacd413d8e9f2cfa7113e2b01113f5cdaab3
Vactor0911 <vactor0911@gmail.com>
2024-06-01 02:58:43 +0900

    Initial commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,10 @@
+import { CssBaseline, Stack } from "@mui/material";
+
+export const App = () => {
+  return (
+    <>
+      <CssBaseline /> {/*CSS 속성을 모두 제거함*/}
+      <Stack></Stack>
+    </>
+  );
+};
